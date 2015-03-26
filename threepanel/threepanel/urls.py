cb665df61f973b8c9fdcc4df710ef200579602d8
Curtis Lassam <classam@saucelabs.com>
2015-03-25 18:13:57 -0700

    files

--- threepanel/threepanel/urls.py
+++ threepanel/threepanel/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'threepanel.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'dashboard.views.home', name='home'),
 
-    url(r'^admin/', include(admin.site.urls)),
 )
