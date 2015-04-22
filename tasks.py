ab314580b74086ea4ef5396c766428839728ba54
Curtis Lassam <classam@saucelabs.com>
2015-04-22 14:19:07 -0700

    install script

--- tasks.py
+++ tasks.py
@@ -1,5 +1,8 @@
 from invoke import task, run
 
+def multiple(*args):
+    return " && ".join(args)
+
 @task
 def vagrant(command):
     run("vagrant {}".format(command))
@@ -17,12 +20,20 @@ def sshc(command):
     vagrant("ssh -c \"{}\"".format(command))
 
 @task
-def devfolder(command):
-    sshc("cd /home/vagrant/synced/threepanel/ && {}".format(command))
+def install():
+    sshc(multiple("sudo apt-get install -y python3 python3-pip",
+                  "sudo pip3 install -r /home/vagrant/synced/requirements.txt --upgrade"))
+
 
 @task
-def pep8():
-    devfolder("pep8 */*.py --ignore=\"E128,E501,E402\"")
+def devfolder(command):
+    sshc(multiple("cd /home/vagrant/synced/threepanel/",
+                  command))
+
+@task
+def lint():
+    devfolder(multiple("pep8 */*.py --ignore=\"E128,E501,E402\"",
+                       "pyflakes */*.py"))
 
 @task
 def dj(command):
