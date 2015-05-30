a65f0c9536918121f5f860efb7f13cfeb31435f0
Curtis Lassam <classam@saucelabs.com>
2015-05-29 21:50:41 -0400

    production settings tweaks

--- threepanel/threepanel/settings.py
+++ threepanel/threepanel/settings.py
@@ -20,7 +20,6 @@ ADMINS = ( ("Curtis", "curtis@lassam.net"), )
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3dypq3o4^%4_7%nzbjo1n_7op524ors3mjurpqa!yh-%jehe8$'
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,44 +48,11 @@ CELERYBEAT_SCHEDULE = {
 CELERY_IGNORE_RESULT = True
 CELERY_DISABLE_RATE_LIMITS = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/tmp/django.log',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'comics': {
-            'handlers': ['file', 'console'],
-            'level': 'INFO',
-        },
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     },
 }
-
-if DEBUG:
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        },
-    }
-
-else:
-    CACHES = {
-        'default': {
-            'BACKEND': 'redis_cache.RedisCache',
-            'LOCATION': '/tmp/redis.sock',
-        },
-    }
-    CACHE_MIDDLEWARE_ALIAS = "default"
-    CACHE_MIDDLEWARE_SECONDS = 60 * 60
 
 # CELERY SETTINGS
 BROKER_URL = 'redis+socket:///tmp/redis.sock'
@@ -134,30 +100,17 @@ WSGI_APPLICATION = 'threepanel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DJANGO_DB_NAME = os.getenv('DJANGO_DB_NAME', 'threepanel')
-DJANGO_DB_USER = os.getenv('DJANGO_DB_USER', 'threepanel')
-DJANGO_DB_HOST = os.getenv('DJANGO_DB_HOST', 'localhost')
-DJANGO_DB_PASSWORD = os.getenv('DJANGO_DB_PASSWORD', 'threepass')
-DJANGO_DB_PORT = os.getenv('DJANGO_DB_PORT', '')
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': DJANGO_DB_NAME,
-        'USER': DJANGO_DB_USER,
-        'PASSWORD': DJANGO_DB_PASSWORD,
-        'HOST': DJANGO_DB_HOST,
-        'PORT': DJANGO_DB_PORT,
+        'NAME': 'threepanel',
+        'USER': 'threepanel',
+        'PASSWORD': 'threepass',
+        'HOST': 'localhost',
     }
 }
 
-if DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
-    # This is a test key
-    MANDRILL_API_KEY = "b8sBJX8OdA2oXrlhTUlCng"
-    MANDRILL_API_KEY = os.getenv('DJANGO_MANDRILL', MANDRILL_API_KEY)
-    pass
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_SUBJECT_PREFIX = "[cube_drone] "
 SERVER_EMAIL = "noreply@cubedrone.com"
 SITE_URL = "http://localhost:8080"
@@ -176,3 +129,9 @@ USE_L10N = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from threepanel.production_settings import *
+    print("Loaded production settings!")
+except ImportError:
+    print("Loading debug settings!")
