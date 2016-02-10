6da83e34751a8cca2b6a28ded7caff92899dfab3
Curtis <curtis@lassam.net>
2016-02-10 01:59:59 -0800

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEPLOY_REPO=`date +%s`-threepanel
+DEPLOY_BRANCH=continuous
+
+mkdir -p deploys
+pushd deploys
+git clone https://github.com/classam/threepanel.git $DEPLOY_REPO
+pushd $DEPLOY_REPO
+git checkout $DEPLOY_BRANCH
+git pull
+
+pyvenv ./threepenv
+source threepenv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+invoke install --production
+
+popd
+popd
