fd153c42c544a361ad7a6cb4fbc49272779aca66
Curtis <curtis@lassam.net>
2016-02-10 18:05:17 -0800

    add git to the mix

--- configuration/provision.sh
+++ configuration/provision.sh
@@ -7,7 +7,7 @@ echo "Virtualenv: $VIRTUALENV"
 sudo apt-get -y update
 
 echo 'Install Dev Tools'
-sudo apt-get install -y ack-grep vim dos2unix
+sudo apt-get install -y ack-grep vim dos2unix git
 
 echo 'Install Build Tools'
 sudo apt-get install -y build-essential gcc
