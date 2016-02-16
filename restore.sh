4808af48041b2f3f676a353828a2a5c8b68f1341
Curtis <curtis@lassam.net>
2016-02-15 20:37:19 -0800

    next is a bad idea, I think

--- restore.sh
+++ restore.sh
@@ -7,7 +7,6 @@ DEPLOY_REPO=$VAGRANT_HOSTNAME
 mkdir -p deploys
 pushd deploys
 git clone https://github.com/classam/threepanel.git $DEPLOY_REPO
-ln -sf $DEPLOY_REPO next
 pushd $DEPLOY_REPO
 git checkout $DEPLOY_BRANCH
 git pull
