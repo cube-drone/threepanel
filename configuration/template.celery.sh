502b52cb6af0ae74c1e8e4819bd680dd833307dc
Curtis <curtis@lassam.net>
2016-03-16 14:59:35 -0700

    maybe a celery with fewer workers will make less open connections to redis

--- configuration/template.celery.sh
+++ configuration/template.celery.sh
@@ -4,4 +4,5 @@ nohup celery worker --app=${DJANGO_PROJECT_SLUG} \
              --loglevel=info \
              --beat \
              --pidfile=${HOME}/celery.pid \
+             --concurrency=1 \
              --logfile=${HOME}/logs/celery.log 2>&1 &
